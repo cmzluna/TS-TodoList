9912abcf092b872fe8f6674c25aa5e65be9d537f
cmzluna <claudiomazzoli@gmail.com>
2022-06-14 20:58:08 -0300

    styles added

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,21 +1,29 @@
 import React from "react";
+import "./TodoList.css";
 
 interface TodoListProps {
   items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
 }
 
 const ToDoList: React.FC<TodoListProps> = (props: TodoListProps) => {
   // TodoListProps is now a placeholder for the type we wish to pass into ToDoList
   return (
     <>
-      <h1>hi</h1>
+      <h1>Simple yet interesting ToDoList</h1>
       <ul>
         {props.items.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
+          <li key={todo.id}>
+            <span>{todo.text}</span>
+            <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </>
   );
 };
+// en todoDeleteHandler debo recibir el id, en bind no me interesa el this, solo el 2do parametro que representa el argumento de la funcion
 
 export default ToDoList;
